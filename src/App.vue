2062e6630817e680888d1d3c106ac811095471eb
Victor81528 <victor81528@hotmail.com>
2020-07-28 09:57:15 +0800

    微調

--- src/App.vue
+++ src/App.vue
@@ -65,22 +65,78 @@
         <div class="feedback-content">
           <div class="feedback-items item-1">
             <img src="/img/avatar_1.png" alt="產品經理">
-            <h1>高雄一口吃</h1>
-            <p>自從吃了這家餐廳以後，我口水連續流了一個多月都沒停，下次不要再被我遇到，不然我一定連他們桌子都啃下去。</p>
-            <h2>產品經理</h2>
+            <div class="word-part">
+              <h1>高雄一口吃</h1>
+              <p>自從吃了這家餐廳以後，我口水連續流了一個多月都沒停，下次不要再被我遇到，不然我一定連他們桌子都啃下去。</p>
+              <h2>--產品經理</h2>
+            </div>
           </div>
           <div class="feedback-items item-2">
             <img src="/img/avatar_2.png" alt="金身道士">
-            <h1>廟口小霸王</h1>
-            <p>自從吃了這家餐廳以後，我口水連續流了一個多月都沒停，下次不要再被我遇到，不然我一定連他們桌子都啃下去。</p>
-            <h2>金身道士</h2>
+            <div class="word-part">
+              <h1>廟口小霸王</h1>
+              <p>自從吃了這家餐廳以後，我口水連續流了一個多月都沒停，下次不要再被我遇到，不然我一定連他們桌子都啃下去。</p>
+              <h2>--金身道士</h2>
+            </div>
           </div>
           <div class="feedback-items item-3">
             <img src="/img/avatar_3.png" alt="美麗萬分">
-            <h1>春天也來了</h1>
-            <p>自從吃了這家餐廳以後，我口水連續流了一個多月都沒停，下次不要再被我遇到，不然我一定連他們桌子都啃下去。</p>
-            <h2>美麗萬分</h2>
+            <div class="word-part">
+              <h1>春天也來了</h1>
+              <p>自從吃了這家餐廳以後，我口水連續流了一個多月都沒停，下次不要再被我遇到，不然我一定連他們桌子都啃下去。</p>
+              <h2>--美麗萬分</h2>
+            </div>
           </div>
+        </div>
+      </div>
+    </div>
+    <div class="booking">
+      <div class="booking-inside">
+        <div class="booking-tittle">
+          <h1>我們的位置不太夠坐<br>要不要先定位呢？</h1>
+        </div>
+        <div class="booking-content">
+          <img src="/img/Map.png" alt="map">
+          <form class="booking-info">
+            <label for="name">姓名</label>
+            <input type="text" name="name" placeholder="六角講師吃尾牙">
+            <label for="tele">電話</label>
+            <input type="text" name="tele" placeholder="02-12345678">
+            <label for="email">信箱</label>
+            <input type="email" name="email" placeholder="test@gmail.com">
+            <label for="email">人數</label>
+            <input type="number" name="number" placeholder="">
+            <label for="vegetarian">是否需要素食</label>
+            <div class="vegetarian">
+              <div class="yes">
+                <input type="radio" name="vegetarian" value="是"><p>是</p>
+              </div>
+              <div class="no">
+                <input type="radio" name="vegetarian" value="否"><p>否</p>
+              </div>
+            </div>
+            <div class="button">
+              <button id="cancel">取消</button>
+              <input id="send" type="submit" value="送出">
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
+    <div class="footer">
+      <div class="footer-inside">
+        <div class="footer-about">
+          <h1>關於 六角西餐廳</h1>
+          <p>Curabitur lobortis id lorem id bibndum. Ut id consectetur magna. Quisque volutpat augue enim, pulvinar lobortis nibh lacinia at. </p>
+        </div>
+        <div class="footer-contact">
+          <h1>聯絡方式</h1>
+          <p>高雄市中正五路到底</p>
+          <p>+886 886 886</p>
+          <p>service@hexschool.com</p>
+        </div>
+        <div class="footer-logo">
+          <h1>六角西餐廳</h1>
         </div>
       </div>
     </div>
