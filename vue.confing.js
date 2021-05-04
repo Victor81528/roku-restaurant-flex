cb71e7c5cd0dee69d4fb476d1c52b35029abf468
翁Victor <43628067+Victor81528@users.noreply.github.com>
2021-05-04 15:21:33 +0800

    no message

--- vue.confing.js
+++ vue.confing.js
@@ -1,9 +1,5 @@
-// module.exports = {
-//   publicPath: process.env.NODE_ENV === 'production'
-//     ? '/roku-restaurant-flex/'
-//     : ''
-// }
-
 module.exports = {
-  publicPath: './'
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/roku-restaurant-flex/'
+    : ''
 }
